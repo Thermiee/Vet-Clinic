ca5ebc7dcce33446dceaaa4eb764f3383798b156
Tee-to <zeezylee4@gmail.com>
2022-04-26 12:24:23 +0100

    create data file

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+DROP TABLE IF EXISTS animals;
+
 CREATE TABLE animals (
-    id INTEGER,
-    name varchar(100),
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg FLOAT,
-    PRIMARY KEY (id)
+    weight_kg FLOAT
 );

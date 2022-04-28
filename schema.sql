aeff11b030043dd00bf0a83187e4b568cb88151c
Thermiee <navyblack16@yahoo.com>
2022-04-28 23:59:02 +0100

    create schema

--- schema.sql
+++ schema.sql
@@ -1,14 +1,36 @@
 /* Database schema to keep the structure of entire database. */
 
 DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
 
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name varchar(100) NOT NULL,
+    species_id INT,
+    owner_id INT,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg FLOAT
 );
 
-ALTER TABLE animals ADD species varchar(255);
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name varchar(100) NOT NULL,
+    Age INT
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL
+);
+
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+SELECT * FROM animals;
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+SELECT * FROM animals;

aa53e27ca275b0f39b5f370eeb41bd4dd20d20b4
Thermiee <navyblack16@yahoo.com>
2022-05-04 15:50:12 +0100

    Add invoice and medical histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,26 @@ CREATE TABLE treatments{
     name VARCHAR(50)
 }
 
+CREATE TABLE invoice_items(
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id SERIAL NOT NULL,
+    treatment_id SERIAL NOT NULL,
+    CONSTRAINT invoices_fk FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+    CONSTRAINT treatment_fk FOREIGN KEY(treatment_id) REFERENCES treaments(id)
+);
+
+CREATE TABLE medical_histories_treatments{
+    medical_history_id SERIAL,
+    treatment_id SERIAL,
+    CONSTRAINT history_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT treatment_fk FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+}
+
+CREATE INDEX invoice_items_index ON invoice_items(invoice_id);
+CREATE INDEX treatements_histories_id_index ON medical_histories_treatements(id);
+CREATE INDEX invoice_medical_history_index ON invoices(medical_history_id);
+CREATE INDEX histories_patients_id_index ON medical_histories(id);
+CREATE INDEX treatements_items_id_index ON invoice_items(id);

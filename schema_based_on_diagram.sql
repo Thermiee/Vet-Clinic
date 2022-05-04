9cf09aface6387adfe4ae738f647555380909272
Khalil <khalilnasri92@gmail.com>
2022-05-04 15:39:50 +0100

    add schema file based on diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,29 @@
+CREATE TABLE patients{
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    date_of_birth DATE NOT NULL
+}
+
+CREATE TABLE medical_histories{
+    id SERIAL PRIMARY KEY,
+     admitted_at TIMESTAMP NOT NULL,
+    patient_id INT NOT NULL,
+    status VARCHAR(50),
+    CONSTRAINT patients_fk FOREIGN KEY (patient_id) REFERENCES patients(id)
+}
+
+CREATE TABLE invoices{
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP NOT NULL,
+    medical_history_id SERIAL NOT NULL,
+    CONSTRAINT history_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
+}
+
+CREATE TABLE treatments{
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(50),
+    name VARCHAR(50)
+}
+
